8a329ace99fe70fdbf589e3764fd6debc37914d6
Sirip12001 <sirishapaidi12@gmail.com>
2025-08-01 17:24:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional: Smooth scroll for nav links
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e){
+    if(this.hash){
+      e.preventDefault();
+      document.querySelector(this.hash).scrollIntoView({behavior: "smooth"});
+    }
+  });
+});
